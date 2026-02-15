cff68225ea6b463d589f01721e556c0c6564c055
dora-explora <sarrafdara@gmail.com>
2026-02-14 16:27:14 -0800

    finished ledges!!

--- level.gd
+++ level.gd
@@ -3,10 +3,9 @@ extends Node3D
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	for ledge in get_tree().get_nodes_in_group("ledge"):
-		print("connecting ", ledge.name)
+		# print("connecting ", ledge.name)
 		ledge.area_entered.connect($Player.on_ledge_entered.bind(ledge))
-		ledge.area_exited.connect($Player.on_ledge_exited.bind(ledge))
-	
+
 func _input(event):
 	if event.is_action_pressed("lclick") && Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- player.gd
+++ player.gd
@@ -3,31 +3,32 @@ extends RigidBody3D
 var speed = 6.
 var acceleration = 10000.
 var aircceleration = 1000.
-var jump_impulse = 5
-var jump_forward_impulse = 5
+var jump_impulse = 5.
+var jump_forward_impulse = 5.
 var h_sens = 0.15
 var v_sens = 0.0025
 var hand_length = 2.
 var pushback = 5000.
 var v_pushback = 0.2
-@export var foot_pushback = 1000.
+var foot_pushback = 1000.
 var arm_min = 30.
-var getup_impulse = 3.
+var getup_impulse = 7.
+var ledge_pullstrength = 1000.
+var ledge_movestrength = 800.
 
 var _on_floor: bool = true
 var _moving: bool = false
 var _handsav := [Vector2.ZERO, Vector2.ZERO]
 var _feetav := [Vector2.ZERO, Vector2.ZERO]
-var _lgrabbing := false
-var _rgrabbing := false
+var _grabbings: Array[Area3D] = [null, null]
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+
 	if _on_floor && linear_velocity.length() < speed:
 		apply_central_force(direction * acceleration * delta)
-		
+
 	if !_on_floor && !direction.is_zero_approx():
 		var horizontal_velocity = Vector3(linear_velocity.x, 0, linear_velocity.z)
 		if horizontal_velocity.length() >= speed:
@@ -44,27 +45,11 @@ func _physics_process(delta: float) -> void:
 	var shift = Input.is_action_pressed("shift")
 	var lclick = Input.is_action_pressed("lclick")
 	var rclick = Input.is_action_pressed("rclick")
-	
-	if shift && lclick:
-		arm_posrot($LHand, 0, delta)
-		arm_pushback($LHand/Arm, delta)
-		$LHand.visible = true
-		$Camera/FakeLHand.visible = false
-	else:
-		$LHand.position = Vector3.ZERO
-		$LHand.visible = false
-		$Camera/FakeLHand.visible = true
-	if shift && rclick:
-		arm_posrot($RHand, 0, delta)
-		arm_pushback($RHand/Arm, delta)
-		$RHand.visible = true
-		$Camera/FakeRHand.visible = false
-	else:
-		$RHand.position = Vector3.ZERO
-		$RHand.visible = false
-		$Camera/FakeRHand.visible = true
-		
-	if not shift && rclick && _moving:
+
+	process_arm(shift && lclick, $LHand, $Camera/FakeLHand, $LedgeLHand, 0, delta)
+	process_arm(shift && rclick, $RHand, $Camera/FakeRHand, $LedgeRHand, 1, delta)
+
+	if not shift && lclick && _moving:
 		leg_posrot($LFoot, 0, delta)
 		leg_pushback($LFoot, 0, delta)
 		$LFoot.visible = true
@@ -80,7 +65,31 @@ func _physics_process(delta: float) -> void:
 	else:
 		$RFoot.visible = false
 		$FakeRFoot.visible = true
-		
+
+func process_arm(held: bool, hand: Node3D, fake: Node3D, ledgehand: Node3D, index: int, delta: float):
+	if _grabbings[index]:
+		if !held:
+			ledgehand.visible = false
+			_grabbings[index] = null
+			hand.position = Vector3.ZERO
+			fake.visible = true
+			return
+		_handsav[index] = Vector2.ZERO
+		hand.visible = false
+		fake.visible = false
+		during_ledge(_grabbings[index], delta)
+
+	elif held:
+		arm_posrot(hand, index, delta)
+		arm_pushback(hand.get_node("Arm"), index, delta)
+		hand.visible = true
+		fake.visible = false
+	else:
+		_handsav[index] = Vector2.ZERO
+		hand.position = Vector3.ZERO
+		hand.visible = false
+		fake.visible = true
+
 func arm_posrot(hand: Node3D, index: int, delta: float):
 	hand.global_position = position + Vector3(0., 1.4, 0.) * basis
 	var hand_error = rotation.y + PI - hand.rotation.y
@@ -103,19 +112,46 @@ func arm_posrot_snap(hand: Node3D):
 	var target = max($Camera.rotation.x, deg_to_rad(-arm_min if !_moving else 0.))
 	var error = -target - arm.rotation.x
 	arm.rotate_x(error)
-	
-func arm_pushback(arm: SpringArm3D, delta: float):
+
+func arm_pushback(arm: SpringArm3D, index: int, delta: float):
 	var length = arm.get_length() - arm.get_hit_length()
 	var pbstrength = pushback * length
+	if _grabbings[(index + 1) % 2]: pbstrength /= 5.
 	var rawpbdir = arm.global_position - position
 	var pbdir = Vector3(-rawpbdir.x, v_pushback * (arm.rotation.x), -rawpbdir.z)
 	apply_central_force(pbstrength * pbdir * delta)
-	
+
 func on_ledge_entered(area: Area3D, ledge: Area3D):
 	if area.name == "LHandArea":
-		_lgrabbing = true
-	apply_central_impulse(Vector3.UP)
-	print(ledge.name)
+		if _grabbings[1] == ledge: return
+		_grabbings[0] = ledge
+		$LHand.visible = false
+		$LedgeLHand.visible = true
+		$LedgeLHand.global_position = ledge.global_position
+		$LedgeLHand.global_rotation = ledge.global_rotation
+	else:
+		if _grabbings[0] == ledge: return
+		_grabbings[1] = ledge;
+		$RHand.visible = true
+		$LedgeRHand.visible = true
+		$LedgeRHand.global_position = ledge.global_position
+		$LedgeRHand.global_rotation = ledge.global_rotation
+
+func during_ledge(ledge: Area3D, delta: float):
+	var diff = ledge.global_position - $Camera.global_position
+	var pdir = diff.normalized()
+	var pstrength = ledge_pullstrength
+	var dist = diff.length()
+	if dist < 1.:
+		pstrength *= (2. - 2. / dist)
+	else:
+		pstrength *= pow(dist - 1., 2.)
+	apply_central_force(pdir * delta * pstrength)
+	var cdir = $Camera.global_basis * Vector3.FORWARD
+	var ddir = pdir - cdir
+	var mdir = Vector3(ddir.x, ddir.y * 1.5, ddir.z)
+	var mstrength = ledge_movestrength
+	apply_central_force(mdir * mstrength * delta)
 
 func leg_posrot(foot: Node3D, index: int, delta: float):
 	foot.global_position = position + Vector3(0., 1.4, 0.) * basis
@@ -140,18 +176,18 @@ func leg_posrot_snap(foot: Node3D):
 	var leg = foot.get_node("Leg")
 	var error = $Camera.rotation.x - leg.rotation.x + 3*PI/4
 	leg.rotate_x(error)
-	
+
 func leg_pushback(foot: Node, index: int, delta: float):
 	var leg = foot.get_node("Leg")
 	var length = (leg.get_length() - leg.get_hit_length()) / leg.get_length()
-	var pbstrength = foot_pushback * pow(length, 0.3)
+	var pbstrength = 1000. * pow(length, 0.3)
 	var xkick = (1 - _feetav[index].x * 2.)
 	var xdir = sin(leg.rotation.x - PI/2) * sin(rotation.y) * xkick
 	var ydir = cos(leg.rotation.x - PI/2) * 2.
 	var zdir = sin(leg.rotation.x - PI/2) * cos(rotation.y) * xkick
 	var pbdir = Vector3(xdir, ydir, zdir)
 	apply_central_force(pbstrength * pbdir * delta)
-	
+
 func _on_timer_timeout():
 	if _moving && _on_floor:
 		apply_central_impulse(Vector3.UP * getup_impulse)
@@ -181,7 +217,6 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 		var normal := state.get_contact_local_normal(i)
 		if normal.dot(Vector3.UP) > 0.99: on_floor = true
 		i += 1
-	_on_floor = on_floor 
+	_on_floor = on_floor
 	if _moving && on_floor && $Timer.is_stopped():
 		$Timer.start()
-	
