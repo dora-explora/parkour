0023108125f56cf4e0a22555792add5c53ec5b26
dora-explora <sarrafdara@gmail.com>
2026-02-16 12:11:53 -0800

    added tutorial and semblance of lava

--- level.gd
+++ level.gd
@@ -6,9 +6,15 @@ func _ready():
 	for ledge in get_tree().get_nodes_in_group("ledge"):
 		# print("connecting ", ledge.name)
 		ledge.area_entered.connect($Player.on_ledge_entered.bind(ledge))
+	$Lava/Area.area_entered.connect(on_lava)
 
 func _input(event):
 	if event.is_action_pressed("lclick") && Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if event.is_action_pressed("exit"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func on_lava(area: Area3D):
+	print(area.name, " has touched the floor")
+	if area.name == "PlayerArea": # kill the playerf
+		$Player.position = Vector3.ZERO
